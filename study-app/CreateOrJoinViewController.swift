e0ae345e042cacf903284fbc72dbf5925457cd83
Katie Catton <kac395@cornell.edu>
2020-12-06 21:49:36 -0500

    add login and creat/join screens

---
+++ study-app/CreateOrJoinViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateOrJoinViewController.swift
+//  study-app
+//
+//  Created by Katie Catton on 12/6/20.
+//
+
+import UIKit
+
+class CreateOrJoinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

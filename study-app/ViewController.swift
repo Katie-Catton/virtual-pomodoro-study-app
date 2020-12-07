4653da37e46ac1ddca0e12a153f4f5c520dbd188
Katie Catton <kac395@cornell.edu>
2020-12-06 19:34:25 -0500

    Initial Commit

---
+++ study-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  study-app
+//
+//  Created by Katie Catton on 12/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

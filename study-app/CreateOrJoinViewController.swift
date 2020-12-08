3857487d4b4e51f21bc84d4ef1913ebcd486cc7d
Katie Catton <kac395@cornell.edu>
2020-12-08 18:23:28 -0500

    update all files

--- study-app/CreateOrJoinViewController.swift
+++ study-app/CreateOrJoinViewController.swift
@@ -21,7 +21,7 @@ class CreateOrJoinViewController: UIViewController {
     // field to input a "room code" to join a session
     var inputCode: UITextField!
     // error message if room code is incorrect
-    var incorrectLabel: UILabel!
+    var incorrectLabel: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,44 +51,49 @@ class CreateOrJoinViewController: UIViewController {
         // *instructionText
         instructionText = UITextView()
         instructionText.translatesAutoresizingMaskIntoConstraints = false
-        instructionText.text = "To start, select an option below for your work session."
+        instructionText.text = "To start, select an option below."
         instructionText.textColor = .black
         instructionText.font = UIFont(name: "HelveticaNeue-Light", size: 20)
+        instructionText.isScrollEnabled = false
         view.addSubview(instructionText)
         
         // *createButton
         createButton = UIButton(type: UIButton.ButtonType.system)
         createButton.translatesAutoresizingMaskIntoConstraints = false
-        createButton.setTitle("Create", for: .normal)
+        createButton.setTitle("Create Meeting", for: .normal)
         createButton.setTitleColor(UIColor(red: 136/255, green: 134/255, blue: 134/255, alpha: 1.0), for: .normal)
         createButton.titleLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 25)
-        createButton.backgroundColor = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1.0)
+        createButton.backgroundColor = .white
+        createButton.layer.borderWidth = 1
+        createButton.layer.borderColor = UIColor(red: 163/255, green: 161/255, blue: 161/255, alpha: 1.0).cgColor
         // shadow underneath button
         createButton.layer.shadowColor = UIColor(red: 136/255, green: 134/255, blue: 134/255, alpha: 1.0).cgColor
-        createButton.layer.shadowOffset = CGSize(width: 0, height: 2)
-        createButton.layer.shadowOpacity = 0.5
+        createButton.layer.shadowOffset = CGSize(width: 0, height: 4)
+        createButton.layer.shadowOpacity = 0.25
         createButton.layer.shadowRadius = 0
         createButton.layer.masksToBounds = false
         createButton.layer.cornerRadius = 29
-        createButton.contentEdgeInsets = UIEdgeInsets(top: 16, left: 88, bottom: 16, right: 88)
+        createButton.contentEdgeInsets = UIEdgeInsets(top: 16, left: 39, bottom: 16, right: 39)
         createButton.addTarget(self, action: #selector(pushSettings), for: .touchUpInside)
         view.addSubview(createButton)
         
         // *joinButton
         joinButton = UIButton(type: UIButton.ButtonType.system)
         joinButton.translatesAutoresizingMaskIntoConstraints = false
-        joinButton.setTitle("Join", for: .normal)
+        joinButton.setTitle("Join Meeting", for: .normal)
         joinButton.setTitleColor(UIColor(red: 136/255, green: 134/255, blue: 134/255, alpha: 1.0), for: .normal)
         joinButton.titleLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 25)
-        joinButton.backgroundColor = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1.0)
+        joinButton.backgroundColor = .white
+        joinButton.layer.borderWidth = 1
+        joinButton.layer.borderColor = UIColor(red: 163/255, green: 161/255, blue: 161/255, alpha: 1.0).cgColor
         // shadow underneath button
         joinButton.layer.shadowColor = UIColor(red: 136/255, green: 134/255, blue: 134/255, alpha: 1.0).cgColor
-        joinButton.layer.shadowOffset = CGSize(width: 0, height: 2)
-        joinButton.layer.shadowOpacity = 0.5
+        joinButton.layer.shadowOffset = CGSize(width: 0, height: 4)
+        joinButton.layer.shadowOpacity = 0.25
         joinButton.layer.shadowRadius = 0
         joinButton.layer.masksToBounds = false
         joinButton.layer.cornerRadius = 29
-        joinButton.contentEdgeInsets = UIEdgeInsets(top: 16, left: 100, bottom: 16, right: 100)
+        joinButton.contentEdgeInsets = UIEdgeInsets(top: 16, left: 47, bottom: 16, right: 47)
         joinButton.addTarget(self, action: #selector(showInputCode), for: .touchUpInside)
         view.addSubview(joinButton)
         
@@ -96,21 +101,21 @@ class CreateOrJoinViewController: UIViewController {
         inputCode = UITextField()
         inputCode.translatesAutoresizingMaskIntoConstraints = false
         // TODO make sure the force unwrapped font attribute make sense
-        inputCode.attributedPlaceholder = NSAttributedString(string: "Enter room code here", attributes: [.foregroundColor: UIColor.gray, .font: UIFont(name: "HelveticaNeue-Light", size: 25) ?? UIFont.systemFont(ofSize: 25)])
+        inputCode.attributedPlaceholder = NSAttributedString(string: "Enter room code here", attributes: [.foregroundColor: UIColor(red: 163/255, green: 161/255, blue: 161/255, alpha: 1.0), .font: UIFont(name: "HelveticaNeue-Light", size: 20) ?? UIFont.systemFont(ofSize: 20)])
         inputCode.textAlignment = .center
-        inputCode.font = UIFont(name: "HelveticaNeue-Light", size: 25)
-        inputCode.textColor = .gray
+        inputCode.font = UIFont(name: "HelveticaNeue-Light", size: 20)
+        inputCode.textColor = UIColor(red: 163/255, green: 161/255, blue: 161/255, alpha: 1.0)
         inputCode.layer.borderWidth = 1
-        inputCode.layer.borderColor = UIColor(red: 136/255, green: 134/255, blue: 134/255, alpha: 1.0).cgColor
+        inputCode.layer.borderColor = UIColor(red: 163/255, green: 161/255, blue: 161/255, alpha: 1.0).cgColor
+        inputCode.layer.cornerRadius = 10
         
         // TODO
         // *incorrectLabel
-        incorrectLabel = UILabel()
+        incorrectLabel = UITextView()
         incorrectLabel.translatesAutoresizingMaskIntoConstraints = false
         incorrectLabel.text = "Incorrect room code.\nPlease try again."
-        // allows multiple lines
-        incorrectLabel.numberOfLines = 0
         incorrectLabel.textColor = .black
+        incorrectLabel.isScrollEnabled = false
         incorrectLabel.font = UIFont(name: "HelveticaNeue-Light", size: 20)
     }
     
@@ -118,8 +123,8 @@ class CreateOrJoinViewController: UIViewController {
         // *welcomeLabel
         NSLayoutConstraint.activate([
             welcomeLabel.heightAnchor.constraint(equalToConstant: 30),
-            welcomeLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 41),
-            welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40)
+            welcomeLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 140),
+            welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 45)
         ])
         
         // *userNameLabel
@@ -132,14 +137,14 @@ class CreateOrJoinViewController: UIViewController {
         // *instructionText
         NSLayoutConstraint.activate([
             instructionText.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 15),
-            instructionText.heightAnchor.constraint(equalToConstant: 60),
+            // instructionText.heightAnchor.constraint(equalToConstant: 60),
             instructionText.leadingAnchor.constraint(equalTo: welcomeLabel.leadingAnchor),
             instructionText.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
         ])
         
         // *createButton
         NSLayoutConstraint.activate([
-            createButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -10),
+            createButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 20),
             createButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
         
@@ -170,11 +175,10 @@ class CreateOrJoinViewController: UIViewController {
         // *inputCode
         NSLayoutConstraint.activate([
             inputCode.topAnchor.constraint(equalTo: joinButton.bottomAnchor, constant: 50),
-            inputCode.heightAnchor.constraint(equalToConstant: 50),
-            inputCode.widthAnchor.constraint(equalTo: createButton.widthAnchor, constant: 10),
+            inputCode.heightAnchor.constraint(equalToConstant: 58),
+            inputCode.widthAnchor.constraint(equalTo: createButton.widthAnchor),
             inputCode.leadingAnchor.constraint(equalTo: createButton.leadingAnchor)
         ])
-        
         // if room code is input correctly, present final screen (helper method)?
     }
 }
--- study-app/ViewController.swift
+++ study-app/ViewController.swift
@@ -1,36 +1,126 @@
-//
 //  ViewController.swift
-//  study-app
+//  virtual-pomodoro-study-app
 //
-//  Created by Katie Catton on 12/6/20.
+//  Created by Matthew Kim on 12/08/20.
+//  Copyright © 2020 Matthew Kim. All rights reserved.
 //
 import UIKit
 
 class ViewController: UIViewController {
 
-    // temporary button added in order to test create or join screen
-    var testButton: UIButton!
+    var nameLabel: UILabel!
+    var emailLabel: UILabel!
+    var emailField: UITextField!
+    var passwordLabel: UILabel!
+    var passwordField: UITextField!
+    var loginButton: UIButton!
+    var signUpButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        view.backgroundColor = .white
         
-        testButton = UIButton()
-        testButton.translatesAutoresizingMaskIntoConstraints = false
-        testButton.setTitle("Login", for: .normal)
-        testButton.addTarget(self, action: #selector(pushCreateOrJoin), for: .touchUpInside)
-        view.addSubview(testButton)
+        setupViews()
+        setupConstraints()
+        
+    }
+    
+    func setupViews() {
+        
+        nameLabel = UILabel()
+        nameLabel.text = "Sign In"
+        nameLabel.textColor = UIColor.black
+        nameLabel.textAlignment = NSTextAlignment.center
+        nameLabel.font = UIFont(name: "HelveticaNeue-Light", size: 30)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        emailLabel = UILabel()
+        emailLabel.text = "Email:"
+        emailLabel.textColor = UIColor.black
+        emailLabel.textAlignment = NSTextAlignment.left
+        emailLabel.font = UIFont(name: "HelveticaNeue-Light", size: 10)
+        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(emailLabel)
+        
+        emailField = UITextField()
+        emailField.text = "Enter your Email"
+        emailField.textColor = UIColor.black
+        emailField.textAlignment = NSTextAlignment.left
+        emailField.font = UIFont(name: "HelveticaNeue-Light", size: 10)
+        emailField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(emailField)
+        
+        passwordLabel = UILabel()
+        passwordLabel.text = "Password:"
+        passwordLabel.textColor = UIColor.black
+        passwordLabel.textAlignment = NSTextAlignment.left
+        passwordLabel.font = UIFont(name: "HelveticaNeue-Light", size: 10)
+        passwordLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(passwordLabel)
+        
+        passwordField = UITextField()
+        passwordField.text = "Enter your Password"
+        passwordField.textColor = UIColor.black
+        passwordField.textAlignment = NSTextAlignment.left
+        passwordField.font = UIFont(name: "HelveticaNeue-Light", size: 10)
+        passwordField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(passwordField)
+        
+        loginButton = UIButton(type: .roundedRect)
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.sizeToFit()
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.setTitleColor(UIColor(red: 136/255, green: 134/255, blue: 134/255, alpha: 1.0), for: .normal)
+        loginButton.backgroundColor = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1.0)
+        loginButton.layer.shadowColor = UIColor(red: 136/255, green: 134/255, blue: 134/255, alpha: 1.0).cgColor
+        loginButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        loginButton.layer.shadowOpacity = 0.5
+        loginButton.layer.shadowRadius = 0
+        loginButton.layer.masksToBounds = false
+        loginButton.layer.cornerRadius = 29
+        loginButton.contentEdgeInsets = UIEdgeInsets(top: 16, left: 88, bottom: 16, right: 88)
+        loginButton.addTarget(self, action: #selector(testLogin), for: .touchUpInside)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginButton)
+        
+        signUpButton = UIButton()
+        signUpButton.setTitle("Sign Up", for: .normal)
+        signUpButton.setTitleColor(.black, for: .normal)
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
+        //signUpButton.addTarget(self, action: #selector(signUp), for: .touchUpInside)
+        view.addSubview(signUpButton)
+    }
+    
+    func setupConstraints() {
         
         NSLayoutConstraint.activate([
-            testButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            testButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        nameLabel.heightAnchor.constraint(equalToConstant: 40),
+        nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+        nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 140)
+        ])
+        // *createButton
+        NSLayoutConstraint.activate([
+            loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 150),
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+              
+              // *joinButton
+        NSLayoutConstraint.activate([
+            signUpButton.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 15),
+            signUpButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
     
-    @objc func pushCreateOrJoin() {
+    func Login() {
+        
+    }
+    
+    @objc func testLogin() {
         let newViewController = CreateOrJoinViewController()
         navigationController?.pushViewController(newViewController, animated: true)
     }
-
 
 }

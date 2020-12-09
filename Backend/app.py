d78943a99fa30f17de907b5e122986aeaaca8d78
Nnamdi Nwagwu <nan46@cornell.edu>
2020-12-09 17:29:08 -0500

    comments

--- Backend/app.py
+++ Backend/app.py
@@ -31,26 +31,34 @@ def hello():
     return render_template('index.html', api_key=key, session_id=session_id, token=token)
 
 #Create a session and add creator to session
+#Might Change this from using user_id in route to not, depending on how we want to handle 
 @app.route("/session/<int:user_id>/", ["POST"])
 def create_session(user_id):
     user = helper.get_user_by_id(user_id)
     if user is None:
         failure_response("Invalid user")
-    
+    #Create new session in OpenTok
     session = opentok.create_session()
     key = api_key
     session_id = session.session_id
     token = opentok.generate_token(session_id)
+
+    #Create new Session Object 
     new_session = Session(session_id=session_id)
+    #Add creator to Session Object
     new_session.append(user)
+    #Push changes to database
     db.session.add(new_session)
     db.session.commit()
+
+    #Gve client parameters necessary to connect
     return success_response({
         'key': api_key,
         'session_id': session_id,
         'token': token 
     })
 
+@app.route("/session/<int:session_id>/")
 
 
 if __name__ == "__main__":

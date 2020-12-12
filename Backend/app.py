91c66c94665b720f721ddc7e13f1644508a8ffbd
Nnamdi Nwagwu <nan46@cornell.edu>
2020-12-11 20:44:11 -0500

    heroku now up and running

--- Backend/app.py
+++ Backend/app.py
@@ -137,5 +137,5 @@ def delete_session(code):
 
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port=port)
